df5ae699a7413eb1ee47f35d7545c4ee4a0cf783
Sanzhar Beishekeev <franki-55@mail.ru>
2024-11-16 16:43:28 +0600

    Создал компонент для создания нового контакта

---
+++ src/components/ContactsForm/ContactsForm.tsx
@@ -0,0 +1,89 @@
+import {useState} from "react";
+import {IContactsForm} from "../../types";
+import {Button, TextField, Typography} from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import * as React from "react";
+
+const initialState = {
+    name: '',
+    phone: '',
+    email: '',
+    photo: '',
+}
+
+const ContactsForm = () => {
+    const [form, setForm] = useState<IContactsForm>(initialState);
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setForm({...form, [e.target.name]: e.target.value});
+    }
+
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    }
+    return (
+        <form onSubmit={onSubmit}>
+            <Typography variant="h4" sx={{flexGrow: 1, textAlign: "center",}}>
+                Form
+            </Typography>
+            <Grid container spacing={2} sx={{mx: "auto", width: "50%", mt: 4, justifyContent: "center"}}>
+                <Grid size={8}>
+                    <TextField
+                        sx={{width: "100%"}}
+                        id="outlined-basic"
+                        label="Name"
+                        name="name"
+                        value={form.name}
+                        variant="outlined"
+                        onChange={onChange}
+                    />
+                </Grid>
+                <Grid size={8}>
+                    <TextField
+                        sx={{width: "100%"}}
+                        id="outlined-basic"
+                        label="Phone"
+                        name="phone"
+                        value={form.phone}
+                        variant="outlined"
+                        onChange={onChange}
+                    />
+                </Grid>
+                <Grid size={8}>
+                    <TextField
+                        sx={{width: "100%"}}
+                        id="outlined-basic"
+                        label="Email"
+                        name="email"
+                        value={form.email}
+                        variant="outlined"
+                        onChange={onChange}
+                    />
+                </Grid>
+                <Grid size={8}>
+                    <TextField
+                        sx={{width: "100%"}}
+                        id="outlined-basic"
+                        label="Photo"
+                        name="photo"
+                        value={form.photo}
+                        variant="outlined"
+                        onChange={onChange}
+                    />
+                </Grid>
+                <Grid size={8}>
+                    {form.photo && (
+                        <img src={form.photo} alt={form.name} style={{width: 250, height: 140}}/>
+                    )}
+                </Grid>
+                <Grid size={8}>
+                <Button type="submit" variant="contained" sx={{width: "100%"}}>
+                        Save
+                    </Button>
+                </Grid>
+            </Grid>
+        </form>
+    );
+};
+
+export default ContactsForm;
