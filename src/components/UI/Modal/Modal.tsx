534a42880e42dbe0c64a946be1aa7ce1e8bb16bb
Sanzhar Beishekeev <franki-55@mail.ru>
2024-11-17 19:47:25 +0600

    При нажатии на контакты сделал показ модального окна с расширенной информацией о контакте, также в этой модалке можно редактировать и удалять контакта

---
+++ src/components/UI/Modal/Modal.tsx
@@ -0,0 +1,93 @@
+import {useAppDispatch, useAppSelector} from "../../../app/hooks.ts";
+import {closeModal} from "../../../store/slices/contactSlice.ts";
+import {Button, IconButton} from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
+import {RootState} from "../../../app/store.ts";
+import PersonIcon from '@mui/icons-material/Person';
+import EmailIcon from '@mui/icons-material/Email';
+import PhoneIcon from '@mui/icons-material/Phone';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import {NavLink} from "react-router-dom";
+
+
+const Modal = () => {
+    const dispatch = useAppDispatch();
+    const isOpenModal = useAppSelector((state: RootState) => state.contact.isOpenModal);
+    const selectedContact = useAppSelector((state: RootState) => state.contact.selectedContact);
+
+
+    if (!isOpenModal) return null;
+
+    return (
+        <div style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+        }}>
+            <div style={{
+                backgroundColor: 'white',
+                padding: 20,
+                borderRadius: 8,
+                boxShadow: '0 4px 10px rgba(0, 0, 0, 0.2)',
+            }}>
+                <IconButton
+                    style={{
+                        marginLeft: 380,
+                        padding: 0,
+                    }}
+                    onClick={() => dispatch(closeModal())}
+                >
+                    <CloseIcon/>
+                </IconButton>
+                {selectedContact && (
+                    <div style={{
+                        display: "grid",
+                        gridTemplateColumns: 'repeat(2, 1fr)',
+                        marginBottom: 20,
+                        alignItems: 'center',
+                        justifyItems: 'center',
+                    }}>
+                        <img src={selectedContact.photo} alt={selectedContact.name}
+                             style={{width: 150, height: 130}}
+                        />
+                        <div style={{width: '100%'}}>
+                            <p>
+                                <PersonIcon style={{marginRight: 8, verticalAlign: 'middle'}}/>
+                                {selectedContact.name}
+                            </p>
+                            <p>
+                                <EmailIcon style={{marginRight: 8, verticalAlign: 'middle'}}/>
+                                {selectedContact.email}
+                            </p>
+                            <p>
+                                <PhoneIcon style={{marginRight: 8, verticalAlign: 'middle'}}/>
+                                {selectedContact.phone}
+                            </p>
+                        </div>
+                        <Button style={{ width: 120}}>
+                            <DeleteIcon/>
+                            Delete
+                        </Button>
+                        <Button to='/contactForm'
+                                component={NavLink}
+                                style={{ width: 120}}
+                                onClick={() => dispatch(closeModal())}
+                        >
+                            <EditIcon style={{marginRight: 8}}/>
+                            Edit
+                        </Button>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
--- src/containers/Contacts/Contacts.tsx
+++ src/containers/Contacts/Contacts.tsx
@@ -1,8 +1,10 @@
 import {useAppDispatch, useAppSelector} from "../../app/hooks.ts";
 import {getContacts} from "../../store/thunks/contactThunks.ts";
-import {getAllContacts} from "../../store/slices/contactSlice.ts";
+import { getAllContacts, openModal} from "../../store/slices/contactSlice.ts";
 import {useEffect} from "react";
 import {Typography} from "@mui/material";
+import Modal from "../../components/UI/Modal/Modal.tsx";
+
 
 const Contacts = () => {
     const dispatch = useAppDispatch();
@@ -14,25 +16,31 @@ const Contacts = () => {
     }, [dispatch])
 
     return (
-        <div style={{maxWidth: 400,border: "6px solid SteelBlue", padding: 10,}}>
+        <div style={{maxWidth: 400, border: "6px solid SteelBlue", padding: 10,}}>
+            <Modal/>
             <Typography variant='h5' style={{textAlign: "center", textTransform: "uppercase"}}>
                 Contacts list
             </Typography>
             {contacts.map((contact) => (
-                <div key={contact.id} style={{
-                    display: "flex",
-                    padding: 10,
-                    gap: 20,
-                    alignItems: "center",
-                    border: "2px solid SteelBlue",
-                    margin: 10 }}>
+                <div key={contact.id}
+                     style={{
+                         display: "flex",
+                         padding: 10,
+                         gap: 20,
+                         alignItems: "center",
+                         borderBottom: "2px solid SteelBlue",
+                         margin: 10,
+                         cursor: "pointer"
+                     }}
+                     onClick={() => dispatch(openModal(contact))}>
                     <img src={contact.photo} alt={contact.name} style={{
                         width: 120,
                         height: 100,
                         border: '2px solid Teal',
                         padding: 4,
-                        borderRadius: '50%'}} />
-                    <p >{contact.name}</p>
+                        borderRadius: '50%'
+                    }}/>
+                    <p>{contact.name}</p>
                 </div>
             ))}
         </div>
